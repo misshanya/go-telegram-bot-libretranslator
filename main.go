698b8c9348b87c960324f23fa00cb8a3180fea7e
misshanya <mishanyadev@gmail.com>
2024-09-22 12:22:32 +0300

    add translation handler

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"github.com/joho/godotenv"
 )
+
+var LibreTranslateUrl string
 
 func main() {
 	err := godotenv.Load()
@@ -18,6 +25,7 @@ func main() {
 	}
 
 	token := os.Getenv("BOT_TOKEN")
+	LibreTranslateUrl = os.Getenv("LIBRETRANSLATE_URL")
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
@@ -28,6 +36,7 @@ func main() {
 	}
 
 	b.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, startHandler)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/translate", bot.MatchTypePrefix, translateHandler)
 
 	b.Start(ctx)
 }
@@ -36,5 +45,39 @@ func startHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
 		Text:   "Привет! Я бот-переводчик. Написан на Go. Для перевода используется API LibreTranslate",
+	})
+}
+
+func translateHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+
+	textToTranslate := strings.TrimSpace(strings.TrimPrefix(update.Message.Text, "/translate"))
+
+	postBody, _ := json.Marshal(map[string]string{
+		"q":      textToTranslate,
+		"source": "ru",
+		"target": "en",
+	})
+	responseBody := bytes.NewBuffer(postBody)
+	resp, err := http.Post(LibreTranslateUrl+"/translate", "application/json", responseBody)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]string
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	translatedText := result["translatedText"]
+
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   translatedText,
 	})
 }
