0ae161dd7feefaaeb90c99281d5c00a0fca8de01
misshanya <mishanyadev@gmail.com>
2024-10-18 20:41:24 +0300

    Middlewares: add callback logging

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -10,7 +10,11 @@ import (
 
 func LogMessage(next bot.HandlerFunc) bot.HandlerFunc {
 	return func(ctx context.Context, b *bot.Bot, update *models.Update) {
-		log.Printf("Message from %v: %v", update.Message.From.FirstName, update.Message.Text)
+		if update.Message != nil {
+			log.Printf("Message from %v: %v", update.Message.From.FirstName, update.Message.Text)
+		} else if update.CallbackQuery != nil {
+			log.Printf("Callback from %v: %v", update.CallbackQuery.From.FirstName, update.CallbackQuery.Data)
+		}
 		next(ctx, b, update)
 	}
 }
