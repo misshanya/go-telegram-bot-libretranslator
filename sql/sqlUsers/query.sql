16fbc16dbfdc4a3ecfa97cdf42e35dba17c95ee7
misshanya <mishanyadev@gmail.com>
2024-10-29 00:05:30 +0300

    Add queries for source_lang and target_lang in user table

--- sql/sqlUsers/query.sql
+++ sql/sqlUsers/query.sql
@@ -15,3 +15,21 @@ UPDATE user
 SET lang_autodetect = NOT lang_autodetect
 WHERE tg_id = ?
 RETURNING *;
+
+-- name: GetSourceLang :one
+SELECT source_lang FROM user
+WHERE tg_id = ? LIMIT 1;
+
+-- name: GetTargetLang :one
+SELECT target_lang FROM user
+WHERE tg_id = ? LIMIT 1;
+
+-- name: SetSourceLang :exec
+UPDATE user
+SET source_lang = ?
+WHERE tg_id = ?;
+
+-- name: SetTargetLang :exec
+UPDATE user
+SET target_lang = ?
+WHERE tg_id = ?;
