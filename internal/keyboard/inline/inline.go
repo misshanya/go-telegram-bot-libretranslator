7d242b50bf50027b83e49d9c5f4e2616fd4ef41b
misshanya <mishanyadev@gmail.com>
2024-10-30 00:15:41 +0300

    Inline kb: realize manual langs selection

--- internal/keyboard/inline/inline.go
+++ internal/keyboard/inline/inline.go
@@ -12,7 +12,12 @@ import (
 )
 
 func InitInlineKeyboard(ctx context.Context, b *bot.Bot, update *models.Update) *inline.Keyboard {
-	return createKeyboard(ctx, b, update.Message.From.ID)
+	kb, err := createKeyboard(ctx, b, update.Message.From.ID)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return kb
 }
 
 func onInlineKeyboardSelect(ctx context.Context, b *bot.Bot, mes models.MaybeInaccessibleMessage, data []byte) {
@@ -22,18 +27,50 @@ func onInlineKeyboardSelect(ctx context.Context, b *bot.Bot, mes models.MaybeIna
 		updateKeyboard(ctx, b, &mes)
 
 	case "lang-from":
-		// TODO: lang-from callback handling
+		currentSourceLang, err := utils.GetSourceLang(ctx, mes.Message.Chat.ID)
+		if err != nil {
+			log.Println(err)
+		}
+		newSourceLang := utils.GetOppositeLang(currentSourceLang)
+		err = utils.SetSourceLang(ctx, mes.Message.Chat.ID, newSourceLang)
+		if err != nil {
+			b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: mes.Message.Chat.ID,
+				Text:   "Не удалось изменить язык оригинала",
+			})
+		}
+		updateKeyboard(ctx, b, &mes)
 
 	case "lang-to":
-		// TODO: lang-to callback handling
+		currentTargetLang, err := utils.GetTargetLang(ctx, mes.Message.Chat.ID)
+		if err != nil {
+			log.Println(err)
+		}
+		newTargetLang := utils.GetOppositeLang(currentTargetLang)
+		err = utils.SetTargetLang(ctx, mes.Message.Chat.ID, newTargetLang)
+		if err != nil {
+			b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID: mes.Message.Chat.ID,
+				Text:   "Не удалось изменить язык перевода",
+			})
+		}
+		updateKeyboard(ctx, b, &mes)
 	}
 }
 
-func createKeyboard(ctx context.Context, b *bot.Bot, uid int64) *inline.Keyboard {
+func createKeyboard(ctx context.Context, b *bot.Bot, uid int64) (*inline.Keyboard, error) {
 	autoDetectChar, autoDetect := getAutoDetectChar(ctx, uid)
 	autoDetectText := fmt.Sprintf("Автоопределение языка: %v", autoDetectChar)
-	langFromText := fmt.Sprintf("Переводить с: %v", "язык")
-	langToText := fmt.Sprintf("Переводить на: %v", "язык")
+	sourceLang, err := utils.GetSourceLang(ctx, uid)
+	if err != nil {
+		return nil, err
+	}
+	targetLang, err := utils.GetTargetLang(ctx, uid)
+	if err != nil {
+		return nil, err
+	}
+	souceLangText := fmt.Sprintf("Переводить с: %v", sourceLang)
+	targetLangText := fmt.Sprintf("Переводить на: %v", targetLang)
 
 	// Basic keyboard
 	kb := inline.New(b, inline.NoDeleteAfterClick()).
@@ -43,17 +80,20 @@ func createKeyboard(ctx context.Context, b *bot.Bot, uid int64) *inline.Keyboard
 	// Add language options if autodetect is false
 	if !autoDetect {
 		kb = kb.Row().
-			Button(langFromText, []byte("lang-from"), onInlineKeyboardSelect).
-			Button(langToText, []byte("lang-to"), onInlineKeyboardSelect)
+			Button(souceLangText, []byte("lang-from"), onInlineKeyboardSelect).
+			Button(targetLangText, []byte("lang-to"), onInlineKeyboardSelect)
 	}
 
-	return kb
+	return kb, nil
 }
 
-func updateKeyboard(ctx context.Context, b *bot.Bot, mes *models.MaybeInaccessibleMessage) {
-	newKb := createKeyboard(ctx, b, mes.Message.Chat.ID)
+func updateKeyboard(ctx context.Context, b *bot.Bot, mes *models.MaybeInaccessibleMessage) error {
+	newKb, err := createKeyboard(ctx, b, mes.Message.Chat.ID)
+	if err != nil {
+		return err
+	}
 
-	_, err := b.EditMessageReplyMarkup(ctx, &bot.EditMessageReplyMarkupParams{
+	_, err = b.EditMessageReplyMarkup(ctx, &bot.EditMessageReplyMarkupParams{
 		ChatID:      mes.Message.Chat.ID,
 		MessageID:   mes.Message.ID,
 		ReplyMarkup: newKb,
@@ -61,6 +101,7 @@ func updateKeyboard(ctx context.Context, b *bot.Bot, mes *models.MaybeInaccessib
 	if err != nil {
 		log.Println("Error when updating keyboard:", err)
 	}
+	return nil
 }
 
 func getAutoDetectChar(ctx context.Context, uid int64) (string, bool) {
